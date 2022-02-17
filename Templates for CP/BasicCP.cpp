ad6cc75e8cd660fafe330143f582d511b6c75bbf
bloodofdragons <92717102+bloodofdragons@users.noreply.github.com>
2022-02-18 01:09:30 +0530

    Create BasicCP.cpp

---
+++ Templates for CP/BasicCP.cpp
@@ -0,0 +1,32 @@
+// Author : RAJAT SHAHI (@blood_of_dragons)
+// The wheels just keeps on turning... The drummers began to drum...
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include"/Users/rajat/Desktop/CPP codes/debug.h"
+#endif
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 42
+#endif
+
+#define endl '\n'
+const int mod = 1e9 + 7;
+#define rep(i,a,b) for(auto i=a;i<=b;i++)
+#define per(i,a,b) for(auto i=a;i>=b;i--)
+using ll = long long;
+using vll = vector<long long>;
+const int def_size = 100007;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+
+	
+	
+
+
+}
