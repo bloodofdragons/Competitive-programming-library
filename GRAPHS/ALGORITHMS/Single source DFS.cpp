d362fbb832d7ad1ac12ae93a2cada31747e8d621
bloodofdragons <92717102+bloodofdragons@users.noreply.github.com>
2022-02-18 23:48:02 +0530

    Update Single source DFS.cpp

--- GRAPHS/ALGORITHMS/Single source DFS.cpp
+++ GRAPHS/ALGORITHMS/Single source DFS.cpp
@@ -1,15 +1,32 @@
 vector<int> visited;
 
 // visited store the comp number of each node
-void dfs(int node, int comp)
+void dfs(int node, int curr_comp)
 {
-    visited[src] = comp;
+    visited[src] = curr_comp;
 
     for (auto && i : g[node])
     {
         if (not visited[i])
         {
-            dfs(i, comp);
+            dfs(i, curr_comp);
+        }
+    }
+}
+
+// n = total number of nodes
+void DFS(int n)
+{
+    visited.resize(n + 1); // graph is 1-index based
+    int comp_cnt = 0;  // total number of connected component
+    int curr_comp = 1; // current component number
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (not visited[i])
+        {
+            dfs(i, curr_comp);
+            comp_cnt++;
         }
     }
 }
