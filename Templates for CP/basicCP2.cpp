106f7d9ad7011a47388c3262631db7dbee4545c4
bloodofdragons <92717102+bloodofdragons@users.noreply.github.com>
2022-02-19 08:15:18 +0530

    Create basicCP2.cpp

---
+++ Templates for CP/basicCP2.cpp
@@ -0,0 +1,54 @@
+// Author : RAJAT SHAHI (@blood_of_dragons)
+// The wheels just keeps on turning... The drummers began to drum...
+
+#include<iostream>
+#include<map>
+#include<set>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<string>
+#include <cstdlib>
+#include <cstring>
+#include<utility>
+#include <cassert>
+#include <complex>
+#include <chrono>
+#include <random>
+#include<climits>
+#include <array>
+#include<bitset>
+#include<algorithm>
+#include<unordered_map>
+#include <unordered_set>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include"C:\Users\rajat shahi\Documents\debug.h"
+#endif
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 42
+#endif
+
+#define endl '\n'
+const int mod = 1e9 + 7;
+#define rep(i,a,b) for(auto i=a;i<=b;i++)
+#define per(i,a,b) for(auto i=a;i>=b;i--)
+using ll = long long;
+using vll = vector<long long>;
+const int def_size = 100007;
+
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(ios::badbit | ios::failbit);
+
+ 	   
+    
+
+}
