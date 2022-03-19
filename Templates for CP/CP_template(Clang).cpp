5e80d17d30fa095051499b32e891d740a5b02f2a
bloodofdragons <92717102+bloodofdragons@users.noreply.github.com>
2022-03-20 01:49:12 +0530

    Create CP_template(Clang).cpp

---
+++ Templates for CP/CP_template(Clang).cpp
@@ -0,0 +1,35 @@
+// Author : RAJAT SHAHI (@blood_of_dragons)
+// Practice se hi hoga sabkuch... 
+#ifdef ONLINE_JUDGE
+#include <bits/stdc++.h>
+#endif
+#ifndef ONLINE_JUDGE
+#include"/Users/rajat/Desktop/CPP codes/bits.h"
+#endif
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include"/Users/rajat/Desktop/CPP codes/debug.h"
+#endif
+#ifdef ONLINE_JUDGE
+#define debug(...) 42
+#endif
+
+#define endl '\n'
+const int mod = 1e9 + 7;
+#define rep(i,a,b) for(auto i=a;i<=b;i++)
+#define per(i,a,b) for(auto i=a;i>=b;i--)
+using ll = long long;
+using vll = vector<long long>;
+const int def_size = 100007;
+
+
+int main()
+{
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+
+  
+
+
+
+}
